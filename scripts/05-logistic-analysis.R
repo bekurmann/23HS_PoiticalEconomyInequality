9244b9106af6fd604026f1e854b57c2b658aff71
Benjamin Kurmann <benjamin@pop-os.localdomain>
2024-06-17 11:30:04 +0200

    continuing with the regression

--- scripts/05-logistic-analysis.R
+++ scripts/05-logistic-analysis.R
@@ -2,8 +2,11 @@
 # analysis
 # ##############################################################
 
+install.packages("car")
+
 library(tidyverse)
 library(here)
+library(car)
 
 # ##############################################################
 # checkout wd
@@ -16,3 +19,37 @@ getwd()
 # ##############################################################
 # loading data
 selects2019 <- readRDS("data/selects2019_logistic.rds")
+
+# ##############################################################
+# log
+
+# removing nas
+cleaned_data <- selects2019 %>%
+  drop_na(gender, age, income, education, gini_steink, turnout)
+
+# regression model
+logistic_model <- glm(turnout ~ gender + age + income + education + gini_steink, 
+                      data = cleaned_data, 
+                      family = binomial(link = "logit"))
+
+# sum
+summary(logistic_model)
+
+# Calculate odds ratios
+exp(coef(logistic_model))
+
+# ##############################################################
+# diagnostics
+
+# multicollinearity
+vif(logistic_model)
+
+# predicted probabilities
+predicted_probabilities <- predict(logistic_model, type = "response")
+
+# adding predictions to the cleaned data frame
+cleaned_data <- cleaned_data %>%
+  mutate(predicted_turnout = predicted_probabilities)
+
+
+
